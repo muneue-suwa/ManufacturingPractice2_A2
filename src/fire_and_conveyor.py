71e9e58c5628c2c5724cf1a544364357c18cfded
Dongsik <dongsiku+git@gmail.com>
2018-10-25 00:04:26 +0900

    Add motor function

--- src/fire_and_conveyor.py
+++ src/fire_and_conveyor.py
@@ -15,6 +15,7 @@ from os import path
 # siren_mp3 = "ambulance-siren2.mp3"
 
 led_number = 17
+conveyour_number = 18
 button_number = 3
 
 
@@ -28,8 +29,10 @@ class FireAndConveyor:
         siren_mp3 = "ambulance-siren2.mp3"
         self.siren_mp3_path = path.join(audiofiles_dir, siren_mp3)
         self.led_siren = LED(led_number)
+        self.motor_conveyor = LED(conveyour_number)
         self.button = Button(button_number)
         self.destruction_wating_time = destruction_wating_time
+        self.conveyor_time = conveyor_time
 
     def fire_and_conveyor(self):
         self.button.wait_for_press()
@@ -50,7 +53,11 @@ class FireAndConveyor:
             sleep(led_time)
             self.led_siren.off()
             sleep(led_time)
+            if i == 5:
+                self.motor_conveyor.on()
         mixer.music.stop()
+        sleep(self.conveyor_time - siren_time)
+        self.motor_conveyor.off()
 
     def conveyor(self):
         pass
