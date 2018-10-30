c6e298a262f1610e6dd8b16eefa08f057c760a4d
Dongsik <dongsiku+git@gmail.com>
2018-10-30 22:56:21 +0900

    Divide classes of explode_and_escape and fire_and_conveyor

--- src/explode_and_escape.py
+++ src/explode_and_escape.py
@@ -7,25 +7,47 @@ Created on Wed Oct 24 20:48:40 2018
 """
 
 from pygame import mixer
-from gpiozero import LED, Button
-from time import sleep
+from gpiozero import LED
+from time import time
 from os import path
 
-led_number = 17
-button_number = 3
+
+class Explode:
+    def __init__(self, led_explode_pin, audiofiles_dir):
+        self.led_explode = LED(led_explode_pin)
+        self.mixer_explode = mixer
+        self.audiofiles_dir = audiofiles_dir
+
+    def on(self):
+        start_time = time()
+        siren_mp3_path = path.join(self.audiofiles_dir, "bomb1.mp3")
+        self.mixer_explode.init()
+        self.mixer_explode.music.load(siren_mp3_path)
+        self.mixer_explode.music.play(1)
+        self.led_explode.blink()
+        print("siren on")
+        return time() - start_time
+
+    def off(self):
+        start_time = time()
+        self.mixer_explode.music.stop()
+        self.led_explode.off()
+        print("siren off")
+        return time() - start_time
 
 
-class ExplodeAndEscape:
-    def __init__(self,
-                 audiofiles_dir,
-                 conveyor_time,
-                 explosion_wating_time,
-                 destroy_crane_time,
-                 remove_stopper_time):
-        siren_mp3 = "bomb1.mp3 "
-        self.bomb_mp3_path = path.join(audiofiles_dir, siren_mp3)
-        self.led_siren = LED(led_number)
-        self.button = Button(button_number)
+class Escape:
+    def __init__(self, motor_escape_pin):
+        self.motor_escape = LED(motor_escape_pin)
 
-    def explode_and_escape():
-        pass
+    def on(self):
+        start_time = time()
+        self.motor_escape.on()
+        print("escape on")
+        return time() - start_time
+
+    def off(self):
+        start_time = time()
+        self.motor_escape.off()
+        print("escape off")
+        return time() - start_time
--- src/fire_and_conveyor.py
+++ src/fire_and_conveyor.py
@@ -7,61 +7,82 @@ Created on Wed Oct 24 20:32:34 2018
 """
 
 from pygame import mixer
-from gpiozero import LED, Button
-from time import sleep
+from gpiozero import LED
+from time import sleep, time
 from os import path
 
 # audiofiles_dir = "../../MP2_A2_audiofiles/"
 # siren_mp3 = "ambulance-siren2.mp3"
 
-led_number = 17
-conveyour_number = 18
-button_number = 3
-
-
-class FireAndConveyor:
-    def __init__(self,
-                 audiofiles_dir,
-                 siren_led_pin_number,
-                 conveyour_pin_number):
-        self.siren = Siren(LED(siren_led_pin_number))
-        self.conveyor = Conveyor(LED(conveyour_number))
-
-    class Conveyor:
-        def __init__(self):
-            self.on = self.conveyor.on
-            self.off = self.conveyor.off
-
-    class Siren(self):
-        pass
 
 class Conveyor:
-    def __init__(self, motor_conveyor):
-        self.motor_conveyor = motor_conveyor
-        self.on = self.motor_conveyor.on
-        self.off = self.motor_conveyor.off
+    def __init__(self, motor_conveyor_pin):
+        self.motor_conveyor = LED(motor_conveyor_pin)
+
+    def on(self):
+        start_time = time()
+        self.motor_conveyor.on()
+        print("conveyor on")
+        return time() - start_time
+
+    def off(self):
+        start_time = time()
+        self.motor_conveyor.off()
+        print("conveyor off")
+        return time() - start_time
 
 
 class Siren:
-    def __init__(self, led_siren, audiofiles_dir):
-        self.led_siren = led_siren
+    def __init__(self, led_siren_pin, audiofiles_dir):
+        self.led_siren = LED(led_siren_pin)
         self.mixer_siren = mixer
         self.audiofiles_dir = audiofiles_dir
 
     def on(self):
-        siren_mp3_path = path.join(self.audiofiles_dir, "ambulance-siren2.mp3")
+        start_time = time()
+        siren_mp3_path = path.join(self.audiofiles_dir,
+                                   "ambulance-siren2.mp3")
         self.mixer_siren.init()
         self.mixer_siren.music.load(siren_mp3_path)  # 12sec
         self.mixer_siren.music.play(-1)
         self.led_siren.blink()
+        print("siren on")
+        return time() - start_time
 
     def off(self):
+        start_time = time()
         self.mixer_siren.music.stop()
         self.led_siren.off()
+        print("siren off")
+        return time() - start_time
+
+
+class Fire:
+    def __init__(self, led_fire_pin):
+        self.led_fire = LED(led_fire_pin)
+
+    def on(self):
+        start_time = time()
+        self.led_fire.on()
+        print("fire on")
+        return time() - start_time
+
+    def off(self):
+        start_time = time()
+        self.led_fire.off()
+        print("fire off")
+        return time() - start_time
+
 
 if __name__ == "__main__":
-    print("start")
-    fac = FireAndConveyor("../../MP2_A2_audiofiles/AudioFiles",
-                          3, 3, 3, 3)
-    fac.fire_and_conveyor()
-    print("END")
+    led_siren_pin_fig = 17
+    conveyour_pin_fig = 18
+
+    conveyor = Conveyor(conveyour_pin_fig)
+    siren = Siren(led_siren_pin_fig, "../../MP2_A2_audiofiles/")
+
+    conveyor.on()
+    siren.on()
+    sleep(10)
+    siren.off()
+    conveyor.off()
