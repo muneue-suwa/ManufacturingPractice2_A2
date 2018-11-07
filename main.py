7d4f80e48f690fceeca5c964e6811f7dc888e316
Dongsik <dongsiku+git@gmail.com>
2018-11-08 00:39:40 +0900

    Fix bugs: main.py: convert INT for rpi-fig

--- main.py
+++ main.py
@@ -15,18 +15,15 @@ from src.read_setting_json import Setting as mp2Setting
 
 audiodir = path.expanduser('~/Git/MP2_A2_audiofiles/AudioFiles')
 pinfig = mp2Setting("pin_fig")
-fc = FireAndConveyor(led_siren_pin=pinfig.setting_json["led"]
-                                                      ["describe_fire_truck"],
+led_pin = pinfig.setting_json["led"]
+motor_pin = pinfig.setting_json["motor"]
+fc = FireAndConveyor(led_siren_pin=int(led_pin["describe_fire_truck"]),
                      audiofiles_dir=audiodir,
-                     motor_conveyor_pin=pinfig.setting_json["motor"]
-                                                           ["move_conveyor"],
-                     led_fire_pin=pinfig.setting_json["led"]["describe_fire"])
-ee =\
-    ExplodeAndEscape(led_explode_pin=pinfig.setting_json["led"]
-                                                        ["describe_explosion"],
-                     audiofiles_dir=audiodir,
-                     motor_escape_pin=pinfig.setting_json["motor"]
-                                                         ["launch_balls"])
+                     motor_conveyor_pin=int(motor_pin["move_conveyor"]),
+                     led_fire_pin=int(led_pin["describe_fire"]))
+ee = ExplodeAndEscape(led_explode_pin=int(led_pin["describe_explosion"]),
+                      audiofiles_dir=audiodir,
+                      motor_escape_pin=int(motor_pin["launch_balls"]))
 
 
 button_pin = pinfig.setting_json["button"]
