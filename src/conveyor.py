1f9e8a6d811444dda10b273eb0427d024271879e
Dongsik <dongsiku+git@gmail.com>
2018-11-21 20:21:10 +0900

    Change for tb6612fng in conveyor.py

--- src/conveyor.py
+++ src/conveyor.py
@@ -6,16 +6,19 @@ Created on Tue Oct 30 23:36:10 2018
 @author: crantu
 """
 
-from gpiozero import LED
 from read_setting_json import Setting
+from tb6612fng import TB6612FNG
 pin_fig = Setting("pin")
 
 
 class Conveyor:
     def __init__(self):
-        self.motor_conveyor =\
-            LED(int(pin_fig.setting_json["motor"]
-                                        ["move_conveyor"]))
+        motor_pin_fig_in1 =\
+            int(pin_fig.setting_json["motor"]["move_conveyor"]["in1"])
+        motor_pin_fig_in2 =\
+            int(pin_fig.setting_json["motor"]["move_conveyor"]["in2"])
+        self.motor_conveyor = TB6612FNG(pin_fig_in1=motor_pin_fig_in1,
+                                        pin_fig_in2=motor_pin_fig_in2)
 
     def on(self):
         self.motor_conveyor.on()
