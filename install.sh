8ca565748120c8b529c795d0e747db9b83b11068
Dongsik <dongsiku+git@gmail.com>
2018-12-05 23:32:02 +0900

    Add make_crontab.py to get username for path of crontab

--- install.sh
+++ install.sh
@@ -4,5 +4,8 @@ INSTALL_SH_FILENAME=`readlink -f $0`
 INSTALL_SH_DIRNAME=`dirname $INSTALL_SH_FILENAME`
 
 mkdir -p $INSTALL_SH_DIRNAME/log
-sudo crontab $INSTALL_SH_DIRNAME/shutdown_button/shutdown_button.crontab
-crontab $INSTALL_SH_DIRNAME/setting_files/mp2.crontab
+
+python3 $INSTALL_SH_DIRNAME/crontab/make_crontab.py
+
+sudo crontab $INSTALL_SH_DIRNAME/crontab/shutdown_button.crontab
+crontab $INSTALL_SH_DIRNAME/crontab/mp2.crontab
