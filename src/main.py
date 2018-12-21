49fea51b8725c2199eab9d0e9e57782581d1e4a7
Dongsik <dongsiku+git@gmail.com>
2018-12-21 23:33:47 +0900

    Add recovery feature to main.py

--- src/main.py
+++ src/main.py
@@ -14,6 +14,7 @@ from time import time, sleep
 from fire_and_conveyor import FireAndConveyor
 from explode_and_escape import ExplodeAndEscape
 from fire import Fire
+from recovery import Recovery
 
 from read_pincfg import ReadPinFig
 pin_fig = ReadPinFig()
@@ -33,10 +34,12 @@ def get_option():
 
 
 def main():
+    # Preparing
     audiodir = path.expanduser('~/Git/MP2_A2_audiofiles/AudioFiles')
     fire = Fire("Arduino")
     fc = FireAndConveyor(audiofiles_dir=audiodir)
     ee = ExplodeAndEscape(audiofiles_dir=audiodir)
+    recovery = Recovery()
 
     first_button = Button(pin_fig.first_button)
     second_button = Button(pin_fig.second_button)
@@ -45,14 +48,16 @@ def main():
 
     option = get_option()
 
+    # Main
     main_status_led.on()
-    print("start")
 
     if option.waitforenter:
         input("Waiting for ENTER key instead of BUTTON1: ")
     else:
         print("Waiting for BUTTON1 press")
         first_button.wait_for_press()
+
+    print("Main start")
     main_status_led.blink()
     init_time = time()
     fire.on()
@@ -66,14 +71,39 @@ def main():
     ee.main(init_time)
     sleep(5)
     fire.off()
+    print("Main end")
 
     if option.finishtime:
         input("Press ENTER key to measure finish time: ")
         print("{:.3f} sec: ".format(time() - init_time), end="")
     main_status_led.off()
-    print("end")
 
-    if option.loop is True:
+    # Recovery
+    recovery_staus_led.on()
+    if option.waitforenter:
+        input("Waiting for ENTER key instead of BUTTON1: ")
+    else:
+        print("Waiting for BUTTON1 press")
+        first_button.wait_for_press()
+    press_button_time = time()
+
+    if option.waitforenter:
+        input("Waiting for ENTER key instead of BUTTON1: ")
+    else:
+        print("Waiting for BUTTON1 release")
+        first_button.wait_for_release()
+
+    if (time() - press_button_time) < 1:
+        print("Recovery start")
+        recovery_staus_led.blink()
+        recovery.main()
+        print("Recovery end")
+    else:
+        print("Recovery was skipped")
+    recovery_staus_led.off()
+
+    # Start main() again for loop
+    if option.loop:
         main()
 
 
