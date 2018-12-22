973e226288835851820df65a981121f9f44bbb38
Dongsik <dongsiku+git@gmail.com>
2018-12-22 20:06:02 +0900

    Change recovery.py: bug with duplicated pin_fig

--- src/recovery.py
+++ src/recovery.py
@@ -8,28 +8,24 @@ Created on Wed Dec  5 17:45:25 2018
 
 from time import sleep
 
-from gpiozero import Servo
-from tb6612fng import TB6612FNG
-from read_pincfg import ReadPinFig
-from read_freqcfg import ReadFreqFig
 from read_setting_json import Setting
 
-pin_fig = ReadPinFig()
-freq_fig = ReadFreqFig()
+from conveyor import Conveyor
+from destroy_coveyor import DestroyCoveyor
+from remove_stopper import RemoveStopper
+
 setting_time = Setting("time")
+
+moveconv = Conveyor()
+destconv = DestroyCoveyor()
+rmstopper = RemoveStopper()
 
 
 class Recovery:
     def __init__(self):
-        self.motor_moveconv = TB6612FNG(pin_fig_in1=pin_fig.moveconv_motorin1,
-                                        pin_fig_in2=pin_fig.moveconv_motorin2,
-                                        pin_fig_pwm=pin_fig.moveconv_motorpwm,
-                                        frequency=freq_fig.moveconv_frequency)
-
-        self.motor_destconv = TB6612FNG(pin_fig_in1=pin_fig.destconv_motorin1,
-                                        pin_fig_in2=pin_fig.destconv_motorin2,
-                                        pin_fig_pwm=pin_fig.destconv_motorpwm,
-                                        frequency=freq_fig.destconv_frequency)
+        self.motor_moveconv = moveconv.motor_moveconv
+        self.motor_destconv = destconv.motor_destconv
+        self.servo = rmstopper.servo
 
     def main(self):
         self.motor_moveconv.ccw()
@@ -47,7 +43,6 @@ class Recovery:
         self.motor_destconv.stop_and_close()
         print("END motor_destconv.ccw()")
         print("START stopper recovery")
-        self.servo = Servo(pin_fig.remove_stopperservo)
         self.servo.min()
         sleep(1)
         print("END stopper recovery")
