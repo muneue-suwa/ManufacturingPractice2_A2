834839c3fbfce8bc09d81064841b63fe4b333249
Dongsik <dongsiku+git@gmail.com>
2019-01-09 21:52:35 +0900

    Fix but: move init_time to after ENTER pressed

--- test/test_conveyor.py
+++ test/test_conveyor.py
@@ -10,10 +10,11 @@ from time import sleep, time
 
 
 def MeasureConveyorTime(conveyor_class):
-    init_time = time()
+    # init_time = time()
     print("Press ENTER key and start to measure time of conveyor")
     input("If you want to stop, press Ctrl+C: ")
     try:
+        init_time = time()
         conveyor_class.on()
         sleep(60)
     except KeyboardInterrupt:
